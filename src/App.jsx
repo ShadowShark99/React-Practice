f4ddccf793059296247d9f914a479a0864422536
ShadowShark99 <kadenwchan@gmail.com>
2025-07-25 11:02:26 -0500

    Create context for Bank Gas Prices

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { createContext, useState } from 'react'
 import './App.css'
 import HelloWorld from './components/HelloWorld'
 import Button from './components/Button'
@@ -6,10 +6,14 @@ import Person from './components/Person'
 import ClassInput from './components/ClassInput'
 import FunctionalInput from './components/FunctionalInput'
 import { Link } from 'react-router-dom'
+import ButtonRef from './components/ButtonRef'
+import Bank from './components/Bank'
 
+export const BankContext = createContext(null);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  
 
   return (
     <>
@@ -22,6 +26,12 @@ function App() {
           </li>
         </ul>
       </nav>
+      <div>Random count var that increments each bank transaction (Gas Prices): {count}</div>
+      <BankContext.Provider value={{count,setCount}}>
+        <Bank></Bank>
+      </BankContext.Provider>
+
+      
     </>
   )
 }
